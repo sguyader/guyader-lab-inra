ecdb33fbda1f4c77fa1f8ed912fe87d69076a71d
Sebastien Guyader <sguyader@gmail.com>
2020-04-16 01:31:01 -0400

    covid

--- Scripts_R_divers/COVID-19/covid_monde_3.R
+++ Scripts_R_divers/COVID-19/covid_monde_3.R
@@ -6,8 +6,9 @@ library(sf)
 library(minpack.lm)
 theme_set(theme_bw())
 
-
+##############################################
 ### Obtention et mise en forme des Données ###
+##############################################
 
 # Télécharger les données de la Johns Hopkins University
 download.file("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv", destfile = "data/covid19_confirmed.csv")
@@ -38,7 +39,8 @@ jhu.covid$date <- as.Date(jhu.covid$date, format="%m/%d/%y")
 jhu.covid <- jhu.covid %>% rename("Province.State" = "Province/State") %>% rename("Country.Region" = "Country/Region")
 
 # Créer une variable identité unique "geounit" qui combine les 2 premières colonnes Province.State et Country.Region
-jhu.covid <- unite(jhu.covid, geounit, c(Province.State, Country.Region), sep="_", remove=F, na.rm=T)
+jhu.covid <- unite(jhu.covid, geounit, c(Country.Region, Province.State), sep="_", remove=F, na.rm=T)
+
 
 ############################################
 ### Cartographie à l'avant-dernière date ###
@@ -55,42 +57,65 @@ bflast <- jhu.covid$date[(length(unique(jhu.covid$date))-1) * nbcountries]
 # données de l'avant-dernière date
 dat <- dplyr::filter(jhu.covid, date == bflast)
 
-
 ggplot(World) +
   geom_sf(aes(fill=admin), alpha=0.4, color="White", size=0.3) +
   scale_fill_discrete(guide=NULL) +
   geom_point(data=dat, aes(x=Long, y=Lat, size=confirmed, colour=deaths, stroke=0), alpha=0.6) +
   scale_colour_gradient(low="#3333CC", high="red") +
   scale_size(range=c(0.5,15)) +
-  labs(title = "Répartition mondiale des cas avérés de COVID-19", x = "Longitude", y = "Latitude") +
+  labs(title = paste("Répartition mondiale des cas avérés de COVID-19 au", bflast), x = "Longitude", y = "Latitude") +
   coord_sf(expand=F)
 
-geom_map() +
-  geom_point(data=jhu.covid, aes(x=Long, y=Lat, size=confirmed, colour=deaths, stroke=0), alpha=0.6) +
-  scale_colour_gradient(low="#3333CC", high="red") +
-  scale_size(range=c(0,15)) +
-  coord_quickmap(xlim=c(-160,180), ylim=c(-55,80)) +
-  labs(title = "Date: {frame_time}", x = "Longitude", y = "Latitude") +
-  theme_bw() +
-  guides(colour = guide_colourbar(label.vjust=0.5, label.theme = element_text(angle = 90)),
-         size = guide_legend(nrow=1)) +
-  theme(legend.position="bottom") +
-  transition_time(date)
 
 ########################################################################
 ### Génération des cartes de la dynamique temporelle par pays/région ###
 ########################################################################
 
-results = tibble(.rows=3)
-results[,1] <- c("b","Asym","xmid")
-formule <- function(Asym,b,date,xmid) { Asym*exp(-exp(-b*(as.numeric(date)-xmid))) }
-newdata = expand.grid(date=as.Date(jhu.covid$date))
+# créer un tableau vide dans le quel les paramètres des courbes ajustées seront écrits
+results <- tibble(params=c("b","Asym","xmid"))
 
+# formule du modèle de gompertz
+gomp.fn <- confirmed ~ Asym*exp(-exp(-b*(as.numeric(date)-xmid)))
+
+# boucle d'ajustements
 for(i in unique(jhu.covid$geounit)) {
-  tryCatch({ # fonction pour intercepter les messages d'erreur et permettre à la boucle de continuer
-    dat <- filter(jhu.covid, geounit == i)
-    model1 <- nls(confirmed ~ formule(Asym,b,date,xmid), data=dat, start=list(b=0.1, Asym=140000, xmid=18350)) 
-    results <- cbind(results, tibble(coef(model1)))
+  tryCatch({ # fonction pour intercepter les erreurs et laisser la boucle de continuer
+    dat <- filter(jhu.covid, geounit == i) # filtrer par région/pays
+    model1 <- nlsLM(gomp.fn, data=dat, start=list(b=0.1, Asym=1.7e4, xmid=1.83e4))  # ajustement nonliéaire avec algorithme Levenberg-Marquardt
+     res <- tibble(coef(model1))
+     names(res) <- i
+     results <- cbind(results, res) # ajouter les paramètres au tableau
+    newdata = expand.grid(date=as.Date(dat$date)) # créer un jeu de dates servant de valeurs explicatives au modèle pour générer les courbes
+    pred <- predict(model1, newdata=newdata) # générer les valeurs prédites par le modèle
+    p <- ggplot(dat) + # générer le graphique confirmed = f(date) et le stocker en mémoire
+      geom_point(aes(x=as.Date(date), y=confirmed)) + # points de données réelles
+      geom_line(aes(x=as.Date(date), y=pred), color="red") + # courbe ajustée
+      scale_x_date(date_breaks="1 week", labels=scales::date_format("%d-%m-%Y")) +
+      theme(axis.text.x = element_text(angle = 90)) +
+      labs(title = paste("Evolution des cas officiels de COVID-19 en", i), x="date", y="nombre de cas confirmés")
+    ggsave(p, filename=paste("plot_",i,".png"),
+           path="dynplots", width=8, height=5, units="in", dpi=72) # sauver le graphique
+  }, error=function(e){cat("ERROR :",i, "\n")}) # afficher la source si erreur
+}
+
+### Cas particulier de l'Australie, le Cananda et la Chine : les données sont uniquement par région,
+### il faut donc faire la somme des régions pour avoir la dynamique au niveau pays
+
+# Filtrage et préparation des données
+australia <- filter(jhu.covid, Country.Region == "Australia") %>% group_by(date) %>% mutate(confirmed = sum(confirmed))
+canada <- filter(jhu.covid, Country.Region == "Canada") %>% group_by(date) %>% mutate(confirmed = sum(confirmed))
+china <- filter(jhu.covid, Country.Region == "China") %>% group_by(date) %>% mutate(confirmed = sum(confirmed))
+
+AusCanChi <- bind_rows(australia,canada,china)
+
+# Boucle pour l'analyse
+for(i in unique(AusCanChi$Country.Region)) {
+  tryCatch({ # fonction qui intercepte les messages d'erreur et laisse la boucle continuer
+    dat <- filter(AusCanChi, Country.Region == i)
+    model1 <- nlsLM(gomp.fn, data=dat, start=list(b=0.1, Asym=1.4e5, xmid=1.83e4))
+    res <- tibble(coef(model1))
+    names(res) <- i
+    results <- cbind(results, res)
     newdata = expand.grid(date=as.Date(dat$date))
     pred <- predict(model1, newdata=newdata)
     p <- ggplot(dat) +
@@ -99,9 +124,13 @@ for(i in unique(jhu.covid$geounit)) {
       scale_x_date(date_breaks="1 week", labels=scales::date_format("%d-%m-%Y")) +
       theme(axis.text.x = element_text(angle = 90)) +
       labs(title = paste("Evolution des cas officiels de COVID-19 en", i), x="date", y="nombre de cas confirmés")
-    ggsave(p, filename=paste("plot_",i,".png"), path="dynplots")
+    ggsave(p, filename=paste("plot_",i,".png"), path="dynplots", width=8, height=5, units="in", dpi=72)
   }, error=function(e){cat("ERROR :",i, "\n")}) # affiche le nom du pays/région qui pose problème
 }
-  
-results
 
+# Tableau de résultats des ajustements
+print(results)
+write.csv(results, "gompertz_param_table.csv")
+
+results.long <- results %>% pivot_longer(-params, names_to = "Country.Region", values_to = "value") %>%
+  group_by(params) %>% summarize(average=mean(value))
